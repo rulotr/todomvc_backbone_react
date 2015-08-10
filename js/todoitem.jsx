b870182e6502a591d01507713332473c9fec7b52
raultr <RAUL.TORRES@DIR-TEC-RAUL.cusaem.gob.mx>
2015-08-10 15:46:25 -0500

    Evento click y uso de state

--- js/todoitem.jsx
+++ js/todoitem.jsx
@@ -11,13 +11,19 @@ var app = app || {};
 (function () {
 	'use strict';
 	app.TodoItem = React.createClass({
-		
+		getInitialState: function () {
+			return {editText: 0};
+		},
+		handleEdit: function () {
+		//	this.props.onEdit();
+			this.setState({editText: this.state.editText + 1 });
+		},
 		render: function () {
 			return (
 				<li className={classNames({completed: this.props.todo.get('completed')})}	>
 					<div className="view">
 						<input	className="toggle" type="checkbox" checked={this.props.todo.get('completed')} />
-						<label>	{this.props.todo.get('title')} </label>
+						<label  onClick={this.handleEdit}>	{this.props.todo.get('title')} {this.state.editText} </label>
 						<button className="destroy"  />
 					</div>
 					<input ref="editField"	className="edit" />
